0ba2caebd67655a8abfd90d16035cf9ebed35191
ivo-rsk <ip@risika.dk>
2024-02-11 18:55:12 +0200

    Adjustments

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,30 +1,36 @@
-const User = require('../model/User');
-const bcrypt = require('bcrypt');
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
-    const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res
+      .status(400)
+      .json({ message: "Email and password are required." });
 
-    // check for duplicate usernames in the db
-    const duplicate = await User.findOne({ username: user }).exec();
-    if (duplicate) return res.sendStatus(409); //Conflict 
+  const user = email;
+  const pwd = password;
 
-    try {
-        //encrypt the password
-        const hashedPwd = await bcrypt.hash(pwd, 10);
+  // check for duplicate usernames in the db
+  const duplicate = await User.findOne({ username: user }).exec();
+  if (duplicate) return res.status(409).json({ message: "Account conflict" });
 
-        //create and store the new user
-        const result = await User.create({
-            "username": user,
-            "password": hashedPwd
-        });
+  try {
+    //encrypt the password
+    const hashedPwd = await bcrypt.hash(pwd, 10);
 
-        console.log(result);
+    //create and store the new user
+    const result = await User.create({
+      username: user,
+      password: hashedPwd,
+    });
 
-        res.status(201).json({ 'success': `New user ${user} created!` });
-    } catch (err) {
-        res.status(500).json({ 'message': err.message });
-    }
-}
+    console.log(result);
+
+    res.status(201).json({ success: `New user ${user} created!` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports = { handleNewUser };
